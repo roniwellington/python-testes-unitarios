89847625d73bf4c293789f127c08c3fdf06ffc58
roniwellington <roniwellington@gmail.com>
2025-05-21 07:10:21 -0300

    entendendo sobre teste unitarios

---
+++ codigo/main.py
@@ -0,0 +1,12 @@
+from bytebank import Funcionario
+
+#lucas = Funcionario('Lucas Carvalho', '13/03/2000', 1000)
+
+#print(lucas.idade())
+
+def teste_idade():
+    funcionario_teste = Funcionario('Teste', '13/03/2000', 1111)
+    print(f'Teste = {funcionario_teste.idade()}')
+    
+    
+teste_idade()
